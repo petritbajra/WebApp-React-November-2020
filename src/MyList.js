0cce01650deb20bf33de69922666a26866bd2ef3
Petrit <petritbajra@gmail.com>
2020-11-24 23:30:35 +0100

    Created a Import and Export for MyList Funcional

---
+++ src/MyList.js
@@ -0,0 +1,16 @@
+//Functional component
+function MyList() {
+    return (
+        <div>
+            <h2>Top Front-End frameworks</h2>
+            <ul>
+                <li>React</li>
+                <li>Angular</li>
+                <li>Vue</li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default MyList;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-//Functional component
-function MyList() {
-    return (
-        <div>
-            <h2>Top Front-End frameworks</h2>
-            <ul>
-                <li>React</li>
-                <li>Angular</li>
-                <li>Vue</li>
-            </ul>
-        </div>
-
-    )
-}
-
-function MyExercise() {
-    return (
-        <div>
-            <h1>Petrit Bajra</h1>
-            <h3>Istog</h3>
-            <ul>
-                <li>JavaScript</li>
-                <li>Vue</li>
-                <li>React</li>
-            </ul>
-        </div>
-    )
-}
+import MyList from './MyList';
 
 
-ReactDom.render(< MyExercise/>,document.getElementById("root"));
+ReactDom.render(< MyList/>,document.getElementById("root"));
