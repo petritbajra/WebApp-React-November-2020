33c8e4b00d6853e52b2bd1a8f0c49cab7346d5e6
Petrit <petritbajra@gmail.com>
2020-11-24 20:50:14 +0100

    Created Index.js File.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <div>
+        <h1>Hello</h1>
+        <h1>Again</h1>
+    </div>,
+ document.getElementById("root"));
