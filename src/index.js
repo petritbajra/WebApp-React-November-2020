569a851a9ab2218b6e893fc24662695688cd7224
Petrit <petritbajra@gmail.com>
2020-11-25 00:45:54 +0100

    Moved MyList to Components Folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import MyList from './MyList';
+import MyList from './components/MyList';
 
 
 ReactDom.render(< MyList/>,document.getElementById("root"));
