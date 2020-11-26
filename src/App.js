1db31cdb89e964565d77e2f5d08f562e4080af2a
Petrit <petritbajra@gmail.com>
2020-11-26 23:49:22 +0100

    Created a Simple APP with Hours Miredita Miremberma

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 
 function App() {
-    const myStyle = {
-        color:"orange",
-        backgroundColor: "red",
-        fontSize: "25px" // nese e vendosim veq 25 apet e mer px
-    }
+        let greeting = "Miremberma";
+        const myStyle = {
+            color:'red'
+        }
+        const date = new Date();
+        const hour = date.getHours();
+        // const hour = 5; // nese dojm me ndrru oren manual
+        if(hour >= 5 && hour < 10) {
+            greeting = 'Mirmengjes';
+            myStyle.color = 'green'
+        } else if (hour >= 10 && hour < 17) {
+            greeting = 'Miredita';
+            myStyle.color = 'orange'
+        }
     return (
         <div>
-        <div style={myStyle}>
-            <h1>Hello</h1>
-        </div>
-        <div style={myStyle}>
-                <h1>Hello</h1>
-        </div>
+            <h1 style={myStyle}>{greeting}</h1>
+            
         </div>
         
     )
