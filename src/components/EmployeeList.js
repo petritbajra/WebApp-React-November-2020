73c0afbcec406e2f0cc71c28556aef41f34b2497
Petrit <petritbajra@gmail.com>
2020-11-28 16:24:16 +0100

    Use a for loop to display employee data.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,40 +1,16 @@
 
 import Employee from "./Employee"
+import employess from "../employees"
 
-const employeeOne = {
-        profileUrl: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
-        firstName: "Maria",
-        lastName: "Smith",
-        title: "Manager",
-        phoneNumber: "+1 123 123 1234",
-        address: "New York, NY"
-}
-
-const employeeTwo = {
-        profileUrl: "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png",
-        firstName: "John",
-        lastName: "Smith",
-        title: "Manager",
-        phoneNumber: "+1 123 123 1234",
-        address: "New York, NY"
-}
-
-const employeeThree = {
-    profileUrl: "https://www.shareicon.net/data/512x512/2016/09/01/822711_user_512x512.png",
-    firstName: "Matthew",
-    lastName: "Johnson",
-    title: "Manager",
-    phoneNumber: "+1 123 123 1234",
-    address: "New York, NY"
-}
 
 function EmployeeList() {
+    const emp = [];
+    for (let i = 0; i < employess.length; i++) {
+        emp.push(<Employee data={employess[i]}/>);
+    }
+
     return (
-        <div>
-            <Employee data= {employeeOne} /> 
-            <Employee data={employeeTwo} /> 
-            <Employee data={employeeThree} /> 
-        </div>
+    <div>{emp}</div>
     )
 }
 
---
+++ src/employees.js
@@ -0,0 +1,24 @@
+export default [{
+    profileUrl: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    firstName: "Maria",
+    lastName: "Smith",
+    title: "Manager",
+    phoneNumber: "+1 123 123 1234",
+    address: "New York, NY"
+},
+{
+    profileUrl: "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png",
+    firstName: "John",
+    lastName: "Smith",
+    title: "Manager",
+    phoneNumber: "+1 123 123 1234",
+    address: "New York, NY"
+},
+{
+profileUrl: "https://www.shareicon.net/data/512x512/2016/09/01/822711_user_512x512.png",
+firstName: "Matthew",
+lastName: "Johnson",
+title: "Manager",
+phoneNumber: "+1 123 123 1234",
+address: "New York, NY"
+} ]
