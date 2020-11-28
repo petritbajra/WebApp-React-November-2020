e0bd57b44d9b256c85b86d51b90a20c506450aaf
Petrit <petritbajra@gmail.com>
2020-11-28 14:48:53 +0100

    Use the same component multiple times with differen data.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Employee from "./components/Employee";
-import Employee2 from "./components/Employee2";
-import Employee3 from "./components/Employee3";
 
 function App() {
         return (
@@ -11,6 +9,24 @@ function App() {
                     profileUrl="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
                     firstName="Maria"
                     lastName="Smith"
+                    title="Manager"
+                    phoneNumber="+1 123 123 1234"
+                    address="New York, NY"
+                 /> 
+
+                <Employee 
+                    profileUrl="https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png"
+                    firstName="John"
+                    lastName="Smith"
+                    title="Manager"
+                    phoneNumber="+1 123 123 1234"
+                    address="New York, NY"
+                 /> 
+
+                <Employee 
+                    profileUrl="https://www.shareicon.net/data/512x512/2016/09/01/822711_user_512x512.png"
+                    firstName="Matthew"
+                    lastName="Johnson"
                     title="Manager"
                     phoneNumber="+1 123 123 1234"
                     address="New York, NY"
