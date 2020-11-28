5041cc533090897f5ba55531ca54bdc61365aaed
Petrit <petritbajra@gmail.com>
2020-11-29 00:56:44 +0100

    Conditional Rendering.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React from "react";
-import ChildComponent from "./components/ChildComponent"
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            count: 10
+            users: []
         }
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick({target: {name}}) {
-        this.setState(prevState => {
-            return {
-                count: prevState.count + (name === 'decrement' ? -1 : 1)
-            }
-        });
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        console.log('Componenet update.');
+    componentDidMount () {
+        setTimeout(() => {
+            this.setState({
+                users: [
+                    'John',
+                    'Matthew',
+                    'Angela',
+                    'Lisa',
+                    'Bob'
+                ]
+            });
+            
+        }, 2000);
     }
 
     render() {
+    const lis = this.state.users.map((e, i) => <li key={i}>{e}</li>);
+
+        if (!this.state.users.length) {
+            return <h3>Loading...</h3>
+        }
+
         return (
-            <div className="center">
-                <h1>{this.state.count}</h1>
-                <button name="decrement" onClick={this.handleClick }>Decrement</button>
-                <button mame="increment" onClick={this.handleClick}>Increment</button>
-                {this.state.count === 11 && <ChildComponent /> }
-                
+            <div>
+
+                List of users
+                <ul>
+                    {lis}
+                </ul>
             </div>
         )
     }
