6b07c1c100fa0826982b8dd3785bf6fc82c44e01
Petrit <petritbajra@gmail.com>
2020-11-28 18:30:45 +0100

    Ternary-Operator

---
+++ ternary-operator.js
@@ -0,0 +1,12 @@
+const loggedIn = true;
+
+const greeting = `User is ${!loggedIn ? 'not ' : ''}logged in.`;
+
+// if (loggedIn) {
+//     greeting = 'User is logged in.';
+// } else {
+//     greeting = 'User is not logged in';
+// }
+
+
+console.log(greeting);
