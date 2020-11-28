9c9499cab81f789babef67128519680f2fc2838e
Petrit <petritbajra@gmail.com>
2020-11-28 17:58:41 +0100

    Convert functional component to class-base-component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React from "react";
 import EmployeeList from "./components/EmployeeList";
 
-function App() {
-        return (
+// function App() {
+    // return (
+    //     <div>
+    //         <h1>HR App</h1>
+    //         <hr />
+    //         <EmployeeList />
+    //     </div>
+    // )
+// }
+
+class App extends React.Component {
+    render() {
+      return (
             <div>
                 <h1>HR App</h1>
                 <hr />
                 <EmployeeList />
             </div>
         )
+    }
 }
 
 export default App;
