a0e16e1293b5d9b691fcfade1f46df6050db993e
Petrit <petritbajra@gmail.com>
2020-11-28 18:12:50 +0100

    initialize state.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import EmployeeList from "./components/EmployeeList";
 
-// function App() {
-    // return (
-    //     <div>
-    //         <h1>HR App</h1>
-    //         <hr />
-    //         <EmployeeList />
-    //     </div>
-    // )
-// }
-
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: "Something else"
+        };
+    }
+
+    returnRandomStuff() {
+        return 2 * 25;
+        }
+
     render() {
+        console.log(this.state.firstName);
+        console.log(this.returnRandomStuff());
       return (
             <div>
                 <h1>HR App</h1>
