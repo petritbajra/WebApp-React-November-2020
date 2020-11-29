c4d8d0d59dc0238f3b4699190322791d0f078781
Petrit <petritbajra@gmail.com>
2020-11-29 19:30:13 +0100

    Fetch data from a remote source.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
 import React from "react";
+import axios from "axios";
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            users: [],
-            isLoading: true
-        }
-    }
+  constructor() {
+     super(); 
+     this.state = {
+       users: [],
+       isLoading: true
+     }
+  }
 
-    componentDidMount () {
-        setTimeout(() => {
-            this.setState({
-                users: [
-                    'John',
-                    'Matthew',
-                    'Angela',
-                    'Lisa',
-                    'Bob'
-                ],
-                isLoading: false
-            });
-            
-        }, 2000);
-    }
 
-    render() {
+    componentDidMount() {
+      /// API CALL
+      axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+          this.setState({
+            users: res.data,
+            isLoading: false
+          });
+      })
+
+      .catch(e => {
+        this.setState({
+            isLoading: false
+        });
+        alert('Could not fetch users.');
+      });
+    }
+    render() { 
+      if (this.state.isLoading) {
+        return <h1>Loading...</h1>
+      }
+
+      const users = this.state.users.map((e, i) => {
+        return (<tr key={i}>
+                <td>{e.name}</td>
+                <td>{e.username}</td>
+                <td>{e.email}</td>
+                <td>{e.phone}</td>
+                <td>{e.website}</td>
+              </tr>)
+      })
+
+
+
         return (
-            <div>
-                {!this.state.isLoading ? (
-                    <div>
-                        List of users
-                        <ul>
-                        {this.state.users.map((e, i) => <li key={i}>{e}</li>)}
-                        </ul>
-                    </div>
-                ) : <h3>Loading...</h3> }
-            </div>
-        )
+          <div>
+            <h2>List of Users</h2>
+            <table>
+              <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Website</th>
+                  </tr>
+              </thead>
+              <tbody>
+                {users}
+              </tbody>
+            </table>
+          </div>
+
+
+      );
     }
-}
+  }
 
 export default App;
